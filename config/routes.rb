c8e13b9f82f4d348eaf170f369c26fe35617cbee
María Jesús Fuenzalida Palma <jechufuenzalida10@gmail.com>
2019-10-28 17:07:55 -0300

    news

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  get 'news/index', to: 'news#index'
-  get 'news/:id', to: 'news#show', as: 'news_show'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   get 'tasks/index', to: 'tasks#index'
   get 'tasks/:id', to: 'tasks#show', as: 'tasks_show'
-  post 'projects/index', to: 'projects#index', as: 'projects_index'
+  get 'projects', to: 'projects#index', as: 'projects'
   get 'projects/:id', to: 'projects#show', as: 'projects_show'
+  get 'news', to: 'news#index', as: 'news'
+  get 'news/:id', to: 'news#show', as: 'news_show'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
