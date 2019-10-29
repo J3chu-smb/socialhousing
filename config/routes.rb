83c40fccac6320a0db29696b9ea180073293d6f5
María Jesús Fuenzalida Palma <jechufuenzalida10@gmail.com>
2019-10-29 14:40:42 -0300

    index tasks + routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   get 'projects', to: 'projects#index', as: 'projects'
-  get 'projects/show', to: 'projects#show', as: 'projects_show'
+  get 'projects/:id', to: 'projects#show', as: 'projects_show'
   get 'tasks/index', to: 'tasks#index'
   get 'tasks/:id', to: 'tasks#show', as: 'tasks_show'
-  post 'projects/index', to: 'projects#index', as: 'projects_index'
-  get 'projects/:id', to: 'projects#show', as: 'projects_show'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
